62ef9c5e8d47048f04d246aaa88b5435c81fba67
meqativ <meqativ@gmail.com>
2024-02-26 23:21:17 +0200

    love

--- src/code/sonicsays.js
+++ src/code/sonicsays.js
@@ -6,13 +6,31 @@ const emojis = \{
         \};
     args = `{tag:args|{args}}`.split(" ");
 {ignore:(async () => {
-    let text = args.join(" "); 
-	if (message?.referenced_message && message.referenced_message.content) text = message.referenced_message.content;
+    let text = args.join(" ");
+	if (message?.referenced_message && message.referenced_message.content) text = message.referenced_message.content; 
+	//^ use replied message as text
+	
 	if (args.length === 1 && args[0] === "") {
 		ctx.usesSONICSAYS ??= {};
 		if (!ctx.usesSONICSAYS[message.author.id]) ctx.usesSONICSAYS[message.author.id] = 0;
 		ctx.usesSONICSAYS[message.author.id] += 1;
         let stuff = message.content.match(/^(.+)? ?(tag|t) ([^ ^\n]+)/);
+        const fprefix = stuff[0]; // full prefix for the tag run [0] eg. "-t sonicsays"
+        const prefix = stuff[1] || ""; //        used bot prefix [1] eg. "-"
+        const commandName = stuff[2]; //       used command name [2] eg. "t" or "tag"
+        const tagName = stuff[3]; //               used tag name [3] eg. "sonicsays" or "hiya" if fprefix is "-t hiya"
+        return `\`\`\`ansi\n[33mUsage:[39m ${prefix}${commandName} ${tagName} [text…]\n`+
+        `\n\`\`\`ansi\n`+
+        `[36mExamples: [39m\n`+
+        [
+            ` ${fprefix}`,
+            ` ${fprefix} explosion`,
+            ` ${fprefix} some more pointless text`,
+            ` ${fprefix} yesterday i ass systed`,
+            ` ${fprefix} why`,
+            ` ${fprefix} hell yeah well done`
+        ].join("\n")+
+`\`\`\``
 		return `${ctx.usesSONICSAYS[message.author.id] < 5 ? emojis.help : emojis.help2} \`${stuff[0]} [text...]\``;
 	}
 	try {
