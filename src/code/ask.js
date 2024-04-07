947de76f0f652e3fe53b5006ba8a22d9646809ee
мекатів <106783764+meqativ@users.noreply.github.com>
2024-04-07 10:02:38 +0300

    Update ask.js

--- src/code/ask.js
+++ src/code/ask.js
@@ -1,4 +1,4 @@
-{js:{ignore:
+{js:{ignore:(()=>{
 ctx.messages ??= {};
 (()=>{
 if (message?.referenced_message?.content?.length > 0) args = args.join(" ").replace(message.referenced_message.content, "").split(" ");
@@ -22,8 +22,10 @@ let tmp = [...messages];
 tmp[0] = {
     by: "system", text: `You're a Discord bot called "Assyst", but only mention it casually. Your current task is to assist the user that goes by the name "${message.author.username.replaceAll("\"", "")}". You only have partial access to the message history, also you have permissions to send links (https://example.com for example) and you have access to the current date/time, which is ${message.timestamp.replace(".000000+00:00", "").replace("T", " ")} GMT. When replying use the language that they're using to communicate with you. You can use emojis, lists and **markdown** in your responses to make them more interesting. Also talk like a real human would (lowercase letters) and try to act less like an ai. If they aren't saying anything try begin by asking them what they want to do. Make preferably concise responses. (about 10 sentences max and 500 characters max)`
 }
-fetch("https://ai.aesthetic.computer/api/ask", {
+const out = await fetch("https://ai.aesthetic.computer/api/ask", {
     method: "POST",
     body: JSON.stringify({ hint: "character", messages: tmp })
-}).then(r => `󠄴󠄴󠄴`+r.text())
-}}
+}).then(r => `󠄴󠄴󠄴`+r.text()) 
+    if (out === "Access Denied") return "The Ai service this tag was using blocked assyst :+1:, maybe it will return to working state, try later.\n(if i find a working service i'll swap to that, you can `-t delete ask` if you want to put a different tag in place)"
+    return out;
+})()}}
