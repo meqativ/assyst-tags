e34f992dee223c9698bcc903b0b65d085167f520
meqativ <meqativ@gmail.com>
2024-04-08 01:56:12 +0300

    Put a warning instead

---
+++ src/code/freaky.js
@@ -0,0 +1,5 @@
+This tag has moved to a different tag.
+Please run the following 2 commands as the owner of this tag:
+-t create customtext {eval:{download:http://meqativ.github.io/assyst-tags/src/code/customtext.js}}
+
+-t edit THIS_TAG'S_NAME {tag:customtext|{args} --face=cursive}
