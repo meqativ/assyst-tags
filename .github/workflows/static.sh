b79952271da3c5724670baf9e0888bb45247290b
мекатів <106783764+meqativ@users.noreply.github.com>
2024-05-14 22:11:06 +0300

    Update static.sh

--- .github/workflows/static.sh
+++ .github/workflows/static.sh
@@ -1,2 +1,3 @@
-sed "s/$(sed 's/\//\\\//g; s/;/\\;/g' ./src/code/modules/codepasta_before.js)/$(sed 's/\//\\\//g; s/;/\\;/g' ./src/code/modules/codepasta.js)/g" src/* -i # codepasta
+echo "s/$(sed 's/\//\\\//g; s/;/\\;/g' ./src/code/modules/codepasta_before.js)/$(sed 's/\//\\\//g; s/;/\\;/g' ./src/code/modules/codepasta.js)/g"
+sed "s/$(sed 's/\//\\\//g; s/;/\\;/g' ./src/code/modules/codepasta_before.js)/$(sed 's/\//\\\//g; s/;/\\;/g' ./src/code/modules/codepasta.js)/g" src/code/*.js src/code/**/*.js -i # codepasta
 # LATER sed '/^\s*\/\//d; s/\(\/\/[^'\'\"]*\)\|\/\*[^*]*\*\///g; s/\/\*.*\*\///g' src/code/*.js src/code/**/*.js -i # removes comments
