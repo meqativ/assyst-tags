a75e9aae0d5e35030082161ded2ee39f235805de
meqativ <meqativ@gmail.com>
2024-05-14 18:28:51 +0300

    push initial renderfont

---
+++ src/code/renderfont.js
@@ -0,0 +1,5 @@
+{js:{ignore:
+throw new Error("codepasta not replaced. if you're seeing this in an assyst response, please contact the owner of the tag (-t info TAG_NAME). if you are the owner, then make sure to use the meqativ.github.io link instead of directly linking to the file (use the guide to create the tag, <https://github.com/meqativ/assyst-tags/>)");
+}
+{args}
+}
