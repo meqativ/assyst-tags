7b752891bb14fa99c5f4123884235555b35f5b45
мекатів <106783764+meqativ@users.noreply.github.com>
2024-05-14 18:18:27 +0300

    Update static.sh

--- .github/workflows/static.sh
+++ .github/workflows/static.sh
@@ -1,3 +1,2 @@
-echo "$(ls)"
 sed "s/$(sed 's/\//\\\//g' ./src/code/modules/codepasta_before.js)/$(sed 's/\//\\\//g' ./src/code/modules/codepasta.js)/g" src/* -i # codepasta
 sed '/^\s*\/\//d; s/\/\/.*//; s/\/\*.*\*\///g' src/* -i # removes comments
