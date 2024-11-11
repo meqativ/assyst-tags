f6408e5eda9c80270b4353af34e137d4f7ee8e7c
мекатів <106783764+meqativ@users.noreply.github.com>
2024-11-11 14:31:53 +0200

    jollied

--- src/code/jolly.js
+++ src/code/jolly.js
@@ -1,18 +1,33 @@
-{js:
+{set:avatar|{if:{js:/<@!?(\d+)>/.test("{tryarg:0}")}|=|true|{avatar:{tryarg:0}}|{avatar:{userid}}}}{js:
     args = args.join(" ").split(" ")
-    let image = `{tag:img|{args}}`;
-    {ignore:
-        (async()=>{  
-            let im = await fetch(image).then(x => x.arrayBuffer()).then(ImageScript.decode); 
-            if(im.width > im.height) im.crop((im.width - im.height)/2, 0, im.height, im.height); 
-            else if(im.height > im.width) im.crop(0, (im.height- im.width)/2, im.width, im.width);   
-            let bg1 = await fetch("https://meqativ.github.io/assyst-tags/src/assets/oulays/jolly.png").then(x => x.arrayBuffer()).then(ImageScript.decode);   
-            let bg2 = await fetch("https://meqativ.github.io/assyst-tags/src/assets/oulays/snowflakes.png").then(x => x.arrayBuffer()).then(ImageScript.decode);  
-            
-            im.composite(bg1.fit(im.width, im.height), 0,0);  
-            if (args.join(" ").includes("--snowflakes")) im.composite(bg2.fit(im.width, im.height), 0,0);  
+    let lastattachment = `{lastattachment}`;
+    let avatarURL = `{get:avatar}`{ignore:
+    function discordue(input) {
+        const em = input.match(/<(a?):\w+:(\d+)>/);
+        if (em) {
+            return `https://cdn.discordapp.com/emojis/${em[2]}.png?size=256&quality=lossless`
+        }
+    }
+    let parsed = discordue(args[0]) | avatarURL
+    const imageURL = message?.attachments?.[0] ? message.attachments.[0].url : parsed
+    (async()=>{  
+        let image = await fetch(imageURL).then(x => x.arrayBuffer()).then(ImageScript.decode); 
+        if(image.width > image.height) image.crop((image.width - image.height)/2, 0, image.height, image.height); 
+        else if(image.height > image.width) image.crop(0, (image.height- image.width)/2, image.width, image.width);   
+        let bg1 = await fetch("https://meqativ.github.io/assyst-tags/src/assets/oulays/jolly.png").then(x => x.arrayBuffer()).then(ImageScript.decode);   
+        let snow = await fetch("https://meqativ.github.io/assyst-tags/src/assets/oulays/snowfall.gif").then(x => x.arrayBuffer()).then(ImageScript.decode);  
         
-            return im.encode()   
-        })();
+        image.composite(bg1.fit(image.width, image.height), 0,0);  
+        if (args.join(" ").includes("--animated")) {
+            const outFrames = Array.from({length:snow.length}).map(()=>outFrameZero.clone());   
+            outFrames.map((f,i)=>{   
+            const snowOverlay = snow[i];   
+            f.composite(snowOverlay.fit(f.width, f.height), 0, 0)   
+            outFrames[i] = ImageScript.Frame.from(f)   
+            })
+        } else image.composite(bg2.fit(image.width, image.height), 0,0);  
+    
+        return image.encode()   
+    })();
     }
 }
